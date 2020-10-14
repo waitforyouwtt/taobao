bdbb35bd65c2ccced92793885c853a7aecdd4bf8
luoxian <wtt19940512577>
2020-10-14 14:00:45 +0800

    redis 工具类的编写

--- src/test/java/com/yidiandian/TaobaoApplicationTests.java
+++ src/test/java/com/yidiandian/TaobaoApplicationTests.java
@@ -1,7 +1,10 @@
 package com.yidiandian;
 
+import com.yidiandian.utils.RedisUtils;
+import org.assertj.core.util.DateUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TaobaoApplicationTests {
 
+	@Autowired
+	RedisUtils redisUtils;
+
+	@Test
+	public void redisGetTest() {
+		Object value = redisUtils.get("hello");
+		System.out.println("得到的redis value:"+value);
+	}
+
 	@Test
 	public void contextLoads() {
+		boolean flag = redisUtils.set("hello","word");
+		System.out.println("得到的redis value:"+flag);
+
 	}
 
 }
